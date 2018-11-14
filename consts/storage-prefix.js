6edd94095380f397dd2b925f3acfe76469e24ca1
bahalool <bahalool@gmail.com>
2018-11-14 16:16:48 +0200

    missing prefix

--- consts/storage-prefix.js
+++ consts/storage-prefix.js
@@ -2,6 +2,8 @@ module.exports = {
     STORAGE_PREFIX: {
         HKUBE: 'hkube',
         HKUBE_RESULTS: 'hkube-results',
-        HKUBE_METADATA: 'hkube-metadata'
+        HKUBE_METADATA: 'hkube-metadata',
+        HKUBE_STORE: 'hkube-store',
+        HKUBE_EXECUTION: 'hkube-execution'
     }
 };
