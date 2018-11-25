f323af50aa0726ade1386979cbf16d7e173a8d26
bahalool <bahalool@gmail.com>
2018-11-25 10:27:03 +0200

    test all adpters

--- tests/mock/log.js
+++ tests/mock/log.js
@@ -1,6 +1,10 @@
 class Logger {
     static GetLogFromContainer() {
-        return { debug: arg => console.log(arg) }; // eslint-disable-line
+        return {
+            debug: (arg) => { }// eslint-disable-line
+        };
+
+        // return { debug: arg => console.log(arg) }; // eslint-disable-line
     }
 }
 module.exports = Logger;
