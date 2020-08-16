2826946bed6b76a2f897ca8251d6eccf83e40198
Yehiyam Livneh <yehiyam@gmail.com>
2020-08-16 14:17:42 +0300

    add persistency bucket

--- tests/tests.js
+++ tests/tests.js
@@ -331,6 +331,62 @@ describe('storage-manager tests', () => {
                         expect(keysAfter.length).to.equal(0);
                     });
                 });
+                describe(name + ':hkube-persistency', () => {
+                    it('get and put string', async () => {
+                        const type = uuid();
+                        const name = uuid();
+                        const storageInfo = await storageManager.hkubePersistency.put({ type, name, data: 'gal-gadot' });
+                        const res1 = await storageManager.get(storageInfo);
+                        const res2 = await storageManager.hkubePersistency.get({ type, name });
+                        expect('gal-gadot').to.equal(res1);
+                        expect('gal-gadot').to.equal(res2);
+                    });
+                    it('get and put object', async () => {
+                        const type = uuid();
+                        const name = uuid();
+                        const storageInfo = await storageManager.hkubePersistency.put({ type, name, data: { test: 'gal-gadot' } });
+                        const res1 = await storageManager.get(storageInfo);
+                        const res2 = await storageManager.hkubePersistency.get({ type, name });
+                        expect({ test: 'gal-gadot' }).to.deep.equal(res1);
+                        expect({ test: 'gal-gadot' }).to.deep.equal(res2);
+                    });
+                    it('get and put array', async () => {
+                        const type = uuid();
+                        const name = uuid();
+                        const storageInfo = await storageManager.hkubePersistency.put({ type, name, data: [1, 2, 3] });
+                        const res1 = await storageManager.get(storageInfo);
+                        const res2 = await storageManager.hkubePersistency.get({ type, name });
+                        expect([1, 2, 3]).to.deep.equal(res1);
+                        expect([1, 2, 3]).to.deep.equal(res2);
+                    });
+                    it('list', async () => {
+                        const type = uuid();
+                        await storageManager.hkubePersistency.put({ type, name: uuid(), data: [1, 2, 3] });
+                        await storageManager.hkubePersistency.put({ type, name: uuid(), data: [1, 2, 3] });
+                        await storageManager.hkubePersistency.put({ type, name: uuid(), data: [1, 2, 3] });
+                        await storageManager.hkubePersistency.put({ type, name: uuid(), data: [1, 2, 3] });
+                        const res = await storageManager.hkubePersistency.list({ type });
+                        expect(res.length).to.equal(4);
+                    });
+                    it('delete', async () => {
+                        const type = uuid();
+                        const taskArray = [uuid(), uuid(), uuid(), uuid()];
+                        await storageManager.hkubePersistency.put({ type, name: taskArray[0], data: 'gal-gadot' });
+                        await storageManager.hkubePersistency.put({ type, name: taskArray[1], data: 'gal-gadot' });
+                        await storageManager.hkubePersistency.put({ type, name: taskArray[2], data: 'gal-gadot' });
+                        await storageManager.hkubePersistency.put({ type, name: taskArray[3], data: 'gal-gadot' });
+
+                        const keys = await storageManager.hkubePersistency.list({ type });
+                        expect(keys.length).to.equal(4);
+
+                        await storageManager.hkubePersistency.delete({ type, name: taskArray[0] });
+                        await storageManager.hkubePersistency.delete({ type, name: taskArray[1] });
+                        await storageManager.hkubePersistency.delete({ type, name: taskArray[2] });
+                        await storageManager.hkubePersistency.delete({ type, name: taskArray[3] });
+                        const keysAfter = await storageManager.hkubePersistency.list({ type });
+                        expect(keysAfter.length).to.equal(0);
+                    });
+                });
                 describe(name + ':hkube-algo-metrics', () => {
                     it('get pipelines and nodes', async () => {
                         if (['redis', 'etcd'].indexOf(name) > -1) {
