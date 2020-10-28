03bb9ece259f49b86641adf8aba910efd5fe4476
reggev <reggev.am@gmail.com>
2020-10-28 16:03:24 +0200

    Datasource (#13)
    * added prefix getter and listWithStats method
    
    * added listWithStats parameters
    
    * Merge branch 'datasource' of github.com:kube-HPC/storage-manager.hkube into datasource
    
    * added tests and type signatures
    
    * Merge branch 'master' into datasource

--- tests/datasource.test.js
+++ tests/datasource.test.js
@@ -16,6 +16,9 @@ const readStreamAsString = (stream) =>
     });
 
 describe('Datasource', () => {
+    it('should get the storage prefix', () => {
+        expect(storageManager.hkubeDataSource.prefix).to.match(/hkube-datasource/i);
+    });
     it('get stream from non existing datasource', async () => {
         const dataSource = uuid.v4();
         const fileName = 'my-file';
@@ -34,8 +37,25 @@ describe('Datasource', () => {
         const res = await readStreamAsString(stream);
         expect(res).to.deep.equal(fileContent);
     });
+    it('should return list with stats', async () => {
+        const dataSource = uuid.v4();
+        const fileNames = new Array(5).fill(0).map(() => uuid.v4()).sort();
+        const readStream = fs.createReadStream('tests/mocks/stream.yml');
+        await Promise.all(
+            fileNames.map(fileName =>
+                storageManager.hkubeDataSource.putStream({ dataSource, fileName, data: readStream })
+            )
+        );
+        const list = await storageManager.hkubeDataSource.listWithStats({ dataSource });
+        expect(list.length).to.be.gte(5);
+        list.forEach(entry => {
+            expect(entry).to.haveOwnProperty('path');
+            expect(entry).to.haveOwnProperty('size');
+            expect(entry).to.haveOwnProperty('mtime');
+        });
+    });
     it('list all the files under a datasource', async () => {
-        const dataSource = uuid.v4()
+        const dataSource = uuid.v4();
         const fileNames = new Array(5).fill(0).map(() => uuid.v4()).sort();
         const readStream = fs.createReadStream('tests/mocks/stream.yml');
         await Promise.all(
@@ -45,15 +65,15 @@ describe('Datasource', () => {
         );
         const list = await storageManager.hkubeDataSource.list({ dataSource });
         const listedNames = list.map(item => item.path).map(item => item.replace(/(.+)\//, '')).sort();
-        expect(listedNames).to.eql(fileNames)
+        expect(listedNames).to.eql(fileNames);
     });
     it('should return an empty list when non existing datasource is listed', async () => {
-        const dataSource = uuid.v4()
+        const dataSource = uuid.v4();
         const list = await storageManager.hkubeDataSource.list({ dataSource });
         expect(list).to.be.empty;
     });
     it('delete a datasource', async () => {
-        const dataSource = uuid.v4()
+        const dataSource = uuid.v4();
         const fileNames = new Array(5).fill(0).map(() => uuid.v4()).sort();
         const readStream = fs.createReadStream('tests/mocks/stream.yml');
         await Promise.all(
