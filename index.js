573bdac24cb3b837b49925fc0ee63f14e5cc4a13
reggev <reggev.am@gmail.com>
2020-10-29 14:19:41 +0200

    added type notation import to avoid missing types

--- index.js
+++ index.js
@@ -1,2 +1,4 @@
 module.exports = require('./lib/storage-manager');
 module.exports.STORAGE_PREFIX = require('./consts/storage-prefix');
+
+/** @typedef {import('./lib/storage-manager')} storageManager */
